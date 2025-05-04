5f1120ab0a378759cc60a6ea184cf57bc0aee2a4
Pingdred <ema.morrone01.em@gmail.com>
2025-05-04 16:22:19 +0200

    Add logic to change agent

---
+++ src/main.py
@@ -0,0 +1,29 @@
+from typing import List, Tuple
+
+from cat.mad_hatter.decorators import hook
+from cat.looking_glass.cheshire_cat import CheshireCat
+from cat.agents.main_agent import MainAgent as CatMainAgent
+from cat.log import log
+
+from .agent.base import BaseAgent
+from .agent.main_agent import MainAgent as NativeFcAgent
+from .settings import BaseSettings, load_allowed_agents
+def _set_agent() -> None:
+    cat = CheshireCat()
+    allowed_agents = load_allowed_agents(cat)
+    # Key is the agent description (Enum value), value is the agent class
+    agents_dict = {agent[2]: agent[0] for agent in allowed_agents}
+    agents_names = {agent[2]: agent[1] for agent in allowed_agents}
+
+    settings = BaseSettings(**cat.mad_hatter.get_plugin().load_settings())
+    SelectedAgent: BaseAgent = agents_dict.get(settings.agent, CatMainAgent)
+
+    if isinstance(cat.main_agent, SelectedAgent):
+        return
+    
+    log.debug(f"AGENT FACTORY: Setting agent to {agents_names[settings.agent]}")
+    cat.main_agent = SelectedAgent()
+
+@hook 
+def before_cat_reads_message(_, cat):
+    _set_agent()
